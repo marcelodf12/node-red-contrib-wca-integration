7df20ec1b91942b596b6ad6e629852cb73285c9f
Marcelo Franco <marcelodf12@gmail.com>
2023-11-25 16:14:46 -0300

    Node for send image from link

--- wca_send_msg.js
+++ wca_send_msg.js
@@ -58,6 +58,56 @@ module.exports = function (RED) {
 
     }
 
+    function wcaSendImageNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        console.log("Init integration");
+
+        const auth = RED.nodes.getNode(config.auth);
+        const token = auth.token
+        const from = auth.from
+        const to = config.destination
+
+        node.on('input', function (msg) {
+            const link = msg.payload.link
+
+            const dest = msg.payload.destination || to
+           
+            var myHeaders = new fetch.Headers();
+            myHeaders.append("Authorization", `Bearer ${token}`);
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+                "messaging_product": "whatsapp",
+                "to": `${dest}`,
+                "type": "image",
+                "image": {
+                    "link": `${link}`
+                }
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            const url = `https://graph.facebook.com/v15.0/${from}/messages`
+
+            fetch(url , requestOptions)
+                .then(response => response.text())
+                .then(result => {console.log(result); node.send(result)})
+                .catch(error => {console.log(error); node.send(error)});
+        });
+
+        node.on('close', function () {
+            console.log("Close integration");
+        });
+
+    }
+
     function wcaSendTemplateNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
@@ -119,5 +169,6 @@ module.exports = function (RED) {
 
     RED.nodes.registerType("wca_send_msg", wcaSendMsgNode);
     RED.nodes.registerType("wca_send_template", wcaSendTemplateNode);
+    RED.nodes.registerType("wca_send_image", wcaSendImageNode);
     RED.nodes.registerType("wca_config", wcaConfig);
 }
